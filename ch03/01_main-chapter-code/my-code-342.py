2d5b49c7a99affc0224b59540a9e48c83a6a6b8a
Rens Kortmann <rens.kortmann@gmail.com>
2026-06-01 17:10:23 +0200

    add code section 342

---
+++ ch03/01_main-chapter-code/my-code-342.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn as nn
+
+class SelfAttention_v1(nn.Module):
+
+    def __init__(self, d_in, d_out):
+        super().__init__()
+
+        self.d_in = d_in
+        self.d_out = d_out
+
+        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_key = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+    
+    def forward(self, x):
+        keys = x @ self.W_key
+        queries = x @ self.W_query
+        values = x@ self.W_value
+
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(
+            attn_scores / self.d_out ** 0.5, dim=-1
+        )
+
+        context_vecs = attn_weights @ values
+        return (context_vecs)
+    
+class SelfAttention_v2(nn.Module):
+
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.d_in = d_in
+        self.d_out = d_out
+
+        self.W_query    = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key      = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value   = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(
+            attn_scores / self.d_out**0.5, dim=-1
+        )
+
+        context_vecs = attn_weights @ values
+        return context_vecs
+
+inputs = torch.tensor(
+  [[0.43, 0.15, 0.89], # Your     (x^1)
+   [0.55, 0.87, 0.66], # journey  (x^2)
+   [0.57, 0.85, 0.64], # starts   (x^3)
+   [0.22, 0.58, 0.33], # with     (x^4)
+   [0.77, 0.25, 0.10], # one      (x^5)
+   [0.05, 0.80, 0.55]] # step     (x^6)
+)
+
+torch.manual_seed(789)
+sa = SelfAttention_v2(3,2)
+
+print(sa(inputs))
